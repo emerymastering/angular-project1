cee5e07f623ff8c49a7a9c9ee9e6bb809c3b5c35
emerymastering <emery@mastering.lt>
2022-12-18 02:30:35 +0100

    clean initial install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'My first project:)';
-}
+export class AppComponent {}
